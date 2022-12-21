723fe39201bf69757ca1c15616ccb007db4bd9ee
Eclipse <virgile.henry@0211@gmail.com>
2022-12-21 10:09:37 +0100

    rework on networking and example

--- src/modules/network.rs
+++ src/modules/network.rs
@@ -1,5 +1,25 @@
 mod server;
 mod client;
+mod packet;
+mod buffer;
+mod serialization;
 
 pub use server::*;
 pub use client::*;
+pub use packet::*;
+pub use serialization::*;
+/*
+A lot of network code is a first implementation, and could be refactored in a better way.
+
+To explain a little :
+A Server is a system for a server,
+A client is a game system for a client,
+packets are what are sent through the network. Even if what is really sent are u8 arrays, 
+packets are nice wrappers around those to easely create, write, read, parse them from any struct implementing :
+NetworkSerializable is a trait on every object that need to be sent through the network.
+network buffer is a wrapper arround a u8 buffer to easely read packets out of raw io streams
+That's it ! to send data, it goes :
+
+struct / enum -> packet -> (client / server).send(packet) -> OS AND RAW CONNECTION
+OS AND RAW CONNECTION -> (client / server).handle -> network buffer -> packet -> struct / enum
+*/
