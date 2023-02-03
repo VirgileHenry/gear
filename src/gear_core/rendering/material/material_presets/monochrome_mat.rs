08025eb3cfada71920f1f4e6147094cee6fe1ba1
Eclipse <virgile.henry@0211@gmail.com>
2023-02-03 11:45:24 +0100

    added as any trait in material properties, and a way to retrieve mat prop from mat

--- src/gear_core/rendering/material/material_presets/monochrome_mat.rs
+++ src/gear_core/rendering/material/material_presets/monochrome_mat.rs
@@ -9,4 +9,12 @@ impl MaterialProperties for MonochromeMaterialProperties {
     fn set_properties_to_shader(&self, shader: &ShaderProgram) {
         shader.set_vec3("color", self.color.as_vector());
     }
+
+    fn as_any(&self) -> &dyn std::any::Any {
+        self
+    }
+
+    fn as_any_mut(&mut self) -> &mut dyn std::any::Any {
+        self
+    }
 }
