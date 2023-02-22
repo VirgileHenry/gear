ab4a928deb118363abd47e1405b7b82fcf2de460
Eclipse <virgile.henry@0211@gmail.com>
2023-02-22 15:21:26 +0100

    reworked material properties : now any number of unique material prop

--- src/gear_core/rendering/geometry/mesh_renderer.rs
+++ src/gear_core/rendering/geometry/mesh_renderer.rs
@@ -21,32 +21,10 @@ impl MeshRenderer {
         // bind the vao for drawing
         self.rendering_buffers.bind();
 
-        // (bind textures)
-        let mut texture_index = self.material.bind_textures(gl::TEXTURE0);
-
-        // (change states)
-
         // draw
         self.rendering_buffers.draw();
 
-        // Unbinding all textures
-        while texture_index != gl::TEXTURE0 {
-            gl::ActiveTexture(texture_index);
-            gl::BindTexture(gl::TEXTURE_2D, 0);
-            texture_index -= 1;
-        }
-
-        // unbinding last gl::TEXTURE0
-        gl::ActiveTexture(texture_index);
-        gl::BindTexture(gl::TEXTURE_2D, 0);
     }
 
-    pub fn attach_texture(&mut self, texture: Texture2D) {
-        self.material.attach_texture(texture);
-    }
-    pub fn pop_texture(&mut self) -> Option<Texture2D> {
-        self.material.pop_texture();
-        self.material.pop_texture()
-    }
 
 }
