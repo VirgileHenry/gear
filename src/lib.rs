17759cde1866bded0f1a4ac21f2a3600a63096e1
Eclipse <virgile.henry@0211@gmail.com>
2023-03-05 17:44:46 +0100

    added matrix2 and 3

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,10 @@ extern crate core;
 pub use cgmath::{
     Deg,
     Euler,
-    Matrix4,
+    Matrix4, Matrix3, Matrix2,
     Quaternion,
     Rad,
-    Vector2,
-    Vector3,
-    Vector4,
+    Vector2, Vector3, Vector4,
 };
 pub use foundry::*;
 pub use foundry;
